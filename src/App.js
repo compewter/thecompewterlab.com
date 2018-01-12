460a89758cb2a36e63921ea1d3131ebebbaada0b
Michael Wetherald <michael@thecompewterlab.com>
2018-01-12 14:43:01 -0800

    Add Twitter logo/link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h2>The Compewter Lab</h2>
-          <a href="https://github.com/compewter"><img src="/assets/images/GitHub-Mark-Light-32px.png" /></a>
+          <a href="https://github.com/compewter"><img className="social-img" src="/assets/images/GitHub-Mark-Light-32px.png" /></a>
+          <a href="https://twitter.com/thecompewterlab"><img className="social-img" width="32px" src="/assets/images/Twitter_Logo_WhiteOnImage.png" /></a>
         </div>
         <p className="App-intro">
           <code>Coming Soon...</code>
